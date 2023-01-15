414cb3a348f07e5efd6734bae6bf1918115b06ea
codeclan student <codeclanstudent@codeclans-MBP-4.lan>
2023-01-15 16:16:48 +0000

    maybe fix

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Home from "./components/Home";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <NavBar />
-          <Routes>
-            <Route path="/portfolio" element={< Home />}></Route>
-            <Route path="/about" element={<About />}></Route>
-            <Route path="/projects" element={<Projects />}></Route>
-          </Routes>
-      </Router>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,20 @@
-import styled from 'styled-components';
-import { Link } from "react-router-dom";
+// import styled from 'styled-components';
+// import { Link } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        
-        <ul>
-          <li>
-            <Link to="/portfolio">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/projects">Projects</Link>
-          </li>
-        </ul>
+        <h1>Whatever</h1>
+        // <ul>
+        //   <li>
+        //     <Link to="/portfolio">Home</Link>
+        //   </li>
+        //   <li>
+        //     <Link to="/about">About</Link>
+        //   </li>
+        //   <li>
+        //     <Link to="/projects">Projects</Link>
+        //   </li>
+        // </ul>
       );
 
 }
