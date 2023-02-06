227576a82062983d822293b19751df4ef5429ffc
sagesaintsalem <85616342+sagesaintsalem@users.noreply.github.com>
2023-02-06 15:06:42 +0000

    Fix React

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React>
+  <React.StrictMode>
     <App />
-  </React>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
