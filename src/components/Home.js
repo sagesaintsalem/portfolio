d2afa482998e4155e9dc08cd6ab37744c91c86d2
sagesaintsalem <85616342+sagesaintsalem@users.noreply.github.com>
2023-02-06 15:05:09 +0000

    Bootstrap attempt

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 // import styled from 'styled-components';
 import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
 
 
 
@@ -8,9 +9,10 @@ const Home = () => {
         <div>
             <h1>Welcome!</h1>
             <p>My name is Rita Toussaint and this is my portfolio site!</p>
-            <Alert key='primary'>
+            <Alert key='primary' width='600px'>
                 <p>I wish I could fix this.</p>
             </Alert>
+            <Button variant="danger">IT BREAK</Button>{' '}
         </div>
     )
 
