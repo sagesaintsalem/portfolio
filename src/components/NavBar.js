7a272484b90d45ffa24d9951e4a8d6bdec9b655e
sagesaintsalem <85616342+sagesaintsalem@users.noreply.github.com>
2023-02-08 15:45:00 +0000

    Add icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,7 +12,8 @@ import Nav from 'react-bootstrap/Nav';
 //   gap: 1em;
 
 // `
-
+ const GHLink = ("https://github.com/sagesaintsalem");
+ const LinkedIn = ("https://www.linkedin.com/in/rita-toussaint/");
 
 
 const NavBar = () => {
@@ -21,8 +22,11 @@ const NavBar = () => {
           <Container>
               <Navbar.Brand href="/portfolio"><img src="./images/chillime.jpg" width="41px" height="56px"/></Navbar.Brand>
               
-              
               <Link to="/projects">Projects</Link>
+
+              <Link to={GHLink}><span src="./images/github.png"></span></Link>
+
+              <Link to={LinkedIn}><span src="./images/linkedblack.png"></span></Link>
            
           </Container>
       
