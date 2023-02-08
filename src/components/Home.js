402c492304ce7803b2e8251853dfe9d5fe888b60
sagesaintsalem <85616342+sagesaintsalem@users.noreply.github.com>
2023-02-08 16:09:12 +0000

    Add home desc

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,20 +10,11 @@ const Home = () => {
 
             <h4>My name is Rita Toussaint and this is my portfolio site!</h4>
             
-            <p>I am a junior full-stack developer with a keen interest in UI design and a background in volunteering
-                and academia. I consider myself to be both an analytical and a creative person and I find that software
-                development is a great way to merge those two aspects of my personality.
+            <p>I originally was an aspiring academic and frequent volunteer before realising that that lifestyle was not for me. During the pandemic, I started to experiment with coding - Python, HTML, CSS, Bootstrap - and found that I not only enjoyed it, but that I was *good* at it. From there, I aimed to build a career in tech: starting with 2 short courses in CyberSecurity at Glasgow Clyde College and then a low-code position at With You With Me. I was, unfortunately, made redundant but I took this bad luck as a chance to kick my coding into high gear and thus be able to secure a job in software development. 
 
-                I recently completed CodeClan's 16-week Full Stack Software Development bootcamp, where I built 3 apps from scratch
-                in the following languages and frameworks - 
+I started at CodeClan in November 2022 and I have gone from having a small amount of coding knowledge to being able to build full-stack websites in a matter of weeks. I have discovered that I have a flair for front-end but I still have skills to share in back-end development too! On top of studying, I have been attending networking events and workshops whilst keeping my LinkedIn page up to date and building my portfolio site (psst - you’re already here!). 
 
-                <ul>
-                    <li>Python with Flask</li>
-                    <li>Javascript with Express and React</li>
-                    <li>Java with Spring</li>
-                </ul>
-
-                This site showcases the best of my work so far.
+I know that this field will be a source of constant learning and I could not be more excited! Learning new things - languages, a new art form, coding - brings me such joy and inspires me. At my core, my biggest wish is to leave this world in a better place than I found it. I hope to use my new skills to improve lives, be that through designing a website or an app for a charity or through helping people protect their data. I sincerely hope that this site will be a springboard for me to achieve that.
             </p>
         </div>
     )
